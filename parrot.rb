80a1054f76d87969f46727a6f6f4ba9257b765bc
Wendy Harris <wendyv510@hotmail.com>
2019-10-12 14:29:24 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot (says="Squawk")
+  puts_and_prints "#{says}"
+end 
+
+
+says="Polly want a cracker"
+parrot("says")
+
